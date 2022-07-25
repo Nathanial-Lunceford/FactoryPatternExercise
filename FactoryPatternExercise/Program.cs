a71c9e6ac4a9bb60df25381567d7977a5daead81
Nate <nathanial.lunceford@gmail.com>
2022-07-25 16:39:14 -0500

    Initial commit

---
+++ FactoryPatternExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using FactoryPatternExercise;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("How many tires do you want your vehicle to have? Write 2 for " +
+            "motorcycle, 4 for car, anything else will give you a car.");
+
+        IVehicle vehicle = VehicleFactory.GetVehicle(Console.ReadLine());
+
+        vehicle.Drive();
+    }
+}
---
+++ FactoryPatternExercise/VehicleFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise
+{
+    public class VehicleFactory
+    {
+        static public IVehicle GetVehicle(string tires)
+        {
+            if(tires == "4")
+            {
+                Car newCar = new Car();
+                return newCar;
+            }
+            else if(tires == "2")
+            {
+                Motorcycle newMotorcycle = new Motorcycle();
+                return newMotorcycle;
+            }
+            else
+            {
+                Car newCar = new Car();
+                return newCar;
+            }
+        }
+    }
+}
